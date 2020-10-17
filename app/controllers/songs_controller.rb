f694be664f71415a2da61f865e69ec5fbf3c23ef
Jorge Ramirez <joramirez96@gmail.com>
2020-10-17 02:01:50 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,12 +1,33 @@
 class SongsController < ApplicationController
+  before_action :set_song!, only:[:show, :edit, :update, :destroy]
 
   def index
     @songs = Song.all
   end
 
   def show
-    
+
   end
+
+  def new
+    @song = Song.new
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+  def destory
+  end
+
+  private
+
+  
+
+
 
 
 end
