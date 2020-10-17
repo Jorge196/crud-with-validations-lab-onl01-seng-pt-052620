8d10e2824fe7406b798e8523352a5fd89c73a842
Jorge Ramirez <joramirez96@gmail.com>
2020-10-17 02:05:36 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,21 +13,38 @@ class SongsController < ApplicationController
     @song = Song.new
   end
 
+  def create
+    @song = Song.new(song_params)
+    if @song.save
+      redirect_to song_path(@song)
+    end
+  end
+
   def edit
   end
 
   def update
-
+    if @song.update(song_params)
+      redirect_to song_path(@song)
+    else
+      render "edit"
+    end
   end
 
   def destory
+    @song.destory
+    redirect_to songs_path
   end
 
   private
 
-  
+  def set_song!
+    @song = Song.find(params[:id])
+  end
 
-
+  def song_params
+    params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
+  end
 
 
 end
